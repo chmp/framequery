98855af02ead997b0b8a8ef97b1d5642c03e1d68
CMP <c.prohm@googlemail.com>
2016-10-13 01:02:48 +0200

    Implement transforms via transform partition

--- src/framequery/_dask.py
+++ src/framequery/_dask.py
@@ -4,6 +4,10 @@ import collections
 import functools as ft
 import operator
 
+import pandas as pd
+import dask.dataframe as dd
+import dask.dataframe.utils
+
 from ._base_executor import BaseExecutor
 from ._dask_util import dataframe_from_scalars
 from ._expression import ExpressionEvaluator
@@ -15,7 +19,6 @@ from ._pandas_util import (
 )
 
 
-# TODO: implement transforms via map_partitions to avoid join overhead
 class DaskExecutor(BaseExecutor, ExpressionEvaluator):
     def __init__(self, id_generator=None, strict=False):
         super(DaskExecutor, self).__init__()
@@ -27,6 +30,25 @@ class DaskExecutor(BaseExecutor, ExpressionEvaluator):
 
         self.functions['ABS'] = abs
         self.functions['POW'] = operator.pow
+
+    def evaluate_transform(self, node, scope):
+        table = self.evaluate(node.table, scope)
+
+        table_id = next(self.id_generator)
+        col_id_expr_pairs = [
+            (column_from_parts(table_id, self._get_selected_column_name(col)), col.value)
+            for col in node.columns
+        ]
+        meta = dd.utils.make_meta([
+            (col_id, self._determine_dtype(expr, table))
+            for (col_id, expr) in col_id_expr_pairs
+        ])
+
+        return table.map_partitions(transform_partitions, col_id_expr_pairs, meta=meta)
+
+    def _determine_dtype(self, expr, table):
+        # NOTE: since no compute is triggered, this operation is cheap
+        return self.evaluate_value(expr, table).dtype
 
     def evaluate_sort(self, node, scope):
         # TODO: implement sorting of dataframes
@@ -51,6 +73,20 @@ class DaskExecutor(BaseExecutor, ExpressionEvaluator):
         columns = list(df.columns)
         df = df.reset_index()
         return df[columns]
+
+
+def transform_partitions(df, col_id_expr_pairs):
+    ex = ExpressionEvaluator()
+    ex.functions['ABS'] = abs
+    ex.functions['POW'] = operator.pow
+
+    result = collections.OrderedDict()
+
+    for col_id, expr in col_id_expr_pairs:
+        result[col_id] = ex.evaluate_value(expr, df)
+
+    return pd.DataFrame(result, index=df.index)
+
 
 def combine_series(items, how='inner'):
     """Helper function to combine mutliple series into a single dataframe.
